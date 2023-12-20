5185c66063dbb9ef9bbe1017a9dd3b78d755d939
anastasia-baklanskaya <rebellenastya7@gmail.com>
2023-12-20 23:28:43 +0300

    add: logo-dark

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
-const lightModeOn = (event) => {
-  navbar.classList.add("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo";
-};
-const lightModeOff = (event) => {
-  navbar.classList.remove("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo-light";
-};
+// const lightModeOn = (event) => {
+//   navbar.classList.add("navbar-light");
+//   logo.href.baseVal = "img/sprite.svg#logo";
+// };
+// const lightModeOff = (event) => {
+//   navbar.classList.remove("navbar-light");
+//   logo.href.baseVal = "img/sprite.svg#logo-light";
+// };
 
 const openMenu = (event) => { // функция открывания меню
   menu.classList.add("is-open"); // вешает класс is-open
@@ -27,7 +27,7 @@ const closeMenu = (event) => { // функция закрывания меню
 };
 
 window.addEventListener("scroll", () => {
-  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+  // this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
